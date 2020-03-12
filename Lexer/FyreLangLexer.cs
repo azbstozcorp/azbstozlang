a41536ab1a9d5c4026b891a83fd98c1f3ea8dfaa
Gavin White <horace5413@gmail.com>
2020-03-12 13:31:14 -0700

    Start on FyreLang

---
+++ Lexer/FyreLangLexer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer {
+    public enum FyreLangToken {
+
+    }
+
+    public class FyreLangLexer : LexerBase<FyreLangToken> {
+        public FyreLangLexer() {
+            AddRules(
+                new List<LexerRule<FyreLangToken>>() {
+
+                });
+        }
+    }
+}
