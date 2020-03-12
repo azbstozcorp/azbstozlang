61a77013e1ce6360d2dcae619691b9d5404a213f
Gavin White <horace5413@gmail.com>
2020-03-12 13:56:34 -0700

    More FyreLang

--- Lexer/FyreLangLexer.cs
+++ Lexer/FyreLangLexer.cs
@@ -4,16 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static Lexer.FyreLangToken;
+
 namespace Lexer {
     public enum FyreLangToken {
+        Unknown = 0,
+        EOF,
 
+        Negative,
+        Positive,
+
+        OParen,
+        CParen,
+        OSquare,
+        CSquare,
+        OBrace,
+        CBrace,
+
+        Integer,
     }
 
     public class FyreLangLexer : LexerBase<FyreLangToken> {
         public FyreLangLexer() {
             AddRules(
                 new List<LexerRule<FyreLangToken>>() {
+                    (Negative, @"-"),
+                    (Positive, @"+"),
 
+                    (OParen, @"\("),
+                    (CParen, @"\)"),
+                    (OSquare, @"\["),
+                    (CSquare, @"\]"),
+                    (OBrace, @"\{"),
+                    (CBrace, @"\}"),
+
+                    (Integer, @"\b\d+\b"),
                 });
         }
     }
